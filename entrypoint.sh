3102b2e58fbe1319129a343d38773f154472dc4f
Milad Nekofar <milad@nekofar.com>
2023-07-27 11:31:39 +0330

    refactor: update script behavior to aid debugging and improve error-handling
    Modified the `entrypoint.sh` script to stop whenever a command fails and enabled a debug mode. Removed the `set -e` command and replaced it with `set -eu` to stop the script execution if any of the commands inside fail. Added a condition to activate the debug mode if the `RUNNER_DEBUG` environment variable is set to `1`. This change is crucial to avoid silent failures and help with debugging by providing verbose output when required.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+# Making sure the script stops if any of the commands fails
+set -eu
+
+# Enable debug mode if RUNNER_DEBUG is 1
+[[ "${RUNNER_DEBUG:-0}" -eq 1 ]] && set -x
 
 # The person who we should greet
 PERSON_TO_GREET="${INPUT_WHO_TO_GREET:=World}"
